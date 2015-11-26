6937d96b476c77118d677ea4f167f2c93e945a50
Steve Moser <contact@stevemoser.org>
2015-11-25 20:37:25 -0500

    Add OSX SDK 10.6 (409.200) Docset changes

--- MyWindowController.m
+++ MyWindowController.m
@@ -1,52 +1,50 @@
 /*
-
-File: MyWindowController.m
-
-Abstract: Source file for this sample's main NSWindowController "TestWindow"
-
-Version: 1.0
-
-Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
-Inc. ("Apple") in consideration of your agreement to the
-following terms, and your use, installation, modification or
-redistribution of this Apple software constitutes acceptance of these
-terms.  If you do not agree with these terms, please do not use,
-install, modify or redistribute this Apple software.
-
-In consideration of your agreement to abide by the following terms, and
-subject to these terms, Apple grants you a personal, non-exclusive
-license, under Apple's copyrights in this original Apple software (the
-"Apple Software"), to use, reproduce, modify and redistribute the Apple
-Software, with or without modifications, in source and/or binary forms;
-provided that if you redistribute the Apple Software in its entirety and
-without modifications, you must retain this notice and the following
-text and disclaimers in all such redistributions of the Apple Software. 
-Neither the name, trademarks, service marks or logos of Apple Inc.
-may be used to endorse or promote products derived from the Apple
-Software without specific prior written permission from Apple.  Except
-as expressly stated in this notice, no other rights or licenses, express
-or implied, are granted by Apple herein, including but not limited to
-any patent rights that may be infringed by your derivative works or by
-other works in which the Apple Software may be incorporated.
-
-The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
-MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
-OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-
-IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
-OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
-MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
-AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-Copyright © 2006-2007 Apple Inc. All Rights Reserved
-
-*/
+     File: MyWindowController.m 
+ Abstract: Sample's main NSWindowController "TestWindow"
+  
+  Version: 1.1 
+  
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
+ Inc. ("Apple") in consideration of your agreement to the following 
+ terms, and your use, installation, modification or redistribution of 
+ this Apple software constitutes acceptance of these terms.  If you do 
+ not agree with these terms, please do not use, install, modify or 
+ redistribute this Apple software. 
+  
+ In consideration of your agreement to abide by the following terms, and 
+ subject to these terms, Apple grants you a personal, non-exclusive 
+ license, under Apple's copyrights in this original Apple software (the 
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple 
+ Software, with or without modifications, in source and/or binary forms; 
+ provided that if you redistribute the Apple Software in its entirety and 
+ without modifications, you must retain this notice and the following 
+ text and disclaimers in all such redistributions of the Apple Software. 
+ Neither the name, trademarks, service marks or logos of Apple Inc. may 
+ be used to endorse or promote products derived from the Apple Software 
+ without specific prior written permission from Apple.  Except as 
+ expressly stated in this notice, no other rights or licenses, express or 
+ implied, are granted by Apple herein, including but not limited to any 
+ patent rights that may be infringed by your derivative works or by other 
+ works in which the Apple Software may be incorporated. 
+  
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
+  
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE. 
+  
+ Copyright (C) 2010 Apple Inc. All Rights Reserved. 
+  
+ */
 
 #import "MyWindowController.h"
 
@@ -57,8 +55,8 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 // -------------------------------------------------------------------------------
 - (void)dealloc
 {
-	[_builtInKeywords release];
-	[_allKeywords release];
+	[builtInKeywords release];
+	[allKeywords release];
 	
     [super dealloc];
 }
@@ -66,7 +64,7 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 // -------------------------------------------------------------------------------
 //	awakeFromNib:
 // -------------------------------------------------------------------------------
--(id)awakeFromNib
+- (void)awakeFromNib
 {
 	// add the searchMenu to this control, allowing recent searches to be added.
 	//
@@ -76,7 +74,7 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 	//
 	if ([searchField respondsToSelector: @selector(setRecentSearches:)])
 	{
-		NSMenu* searchMenu = [[[NSMenu alloc] initWithTitle:@"Search Menu"] autorelease];
+		NSMenu *searchMenu = [[[NSMenu alloc] initWithTitle:@"Search Menu"] autorelease];
 		[searchMenu setAutoenablesItems:YES];
 		
 		// first add our custom menu item (Important note: "action" MUST be valid or the menu item is disabled)
@@ -89,30 +87,30 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 		NSMenuItem *separator =  [NSMenuItem separatorItem];
 		[searchMenu insertItem:separator atIndex:1];
 
-		NSMenuItem* recentsTitleItem = [[NSMenuItem alloc] initWithTitle:@"Recent Searches" action:nil keyEquivalent:@""];
+		NSMenuItem *recentsTitleItem = [[NSMenuItem alloc] initWithTitle:@"Recent Searches" action:nil keyEquivalent:@""];
 		// tag this menu item so NSSearchField can use it and respond to it appropriately
 		[recentsTitleItem setTag:NSSearchFieldRecentsTitleMenuItemTag];
 		[searchMenu insertItem:recentsTitleItem atIndex:2];
 		[recentsTitleItem release];
 		
-		NSMenuItem* norecentsTitleItem = [[NSMenuItem alloc] initWithTitle:@"No recent searches" action:nil keyEquivalent:@""];
+		NSMenuItem *norecentsTitleItem = [[NSMenuItem alloc] initWithTitle:@"No recent searches" action:nil keyEquivalent:@""];
 		// tag this menu item so NSSearchField can use it and respond to it appropriately
 		[norecentsTitleItem setTag:NSSearchFieldNoRecentsMenuItemTag];
 		[searchMenu insertItem:norecentsTitleItem atIndex:3];
 		[norecentsTitleItem release];
 		
-		NSMenuItem* recentsItem = [[NSMenuItem alloc] initWithTitle:@"Recents" action:nil keyEquivalent:@""];
+		NSMenuItem *recentsItem = [[NSMenuItem alloc] initWithTitle:@"Recents" action:nil keyEquivalent:@""];
 		// tag this menu item so NSSearchField can use it and respond to it appropriately
 		[recentsItem setTag:NSSearchFieldRecentsMenuItemTag];	
 		[searchMenu insertItem:recentsItem atIndex:4];
 		[recentsItem release];
 		
-		NSMenuItem* separatorItem = (NSMenuItem*)[NSMenuItem separatorItem];
+		NSMenuItem *separatorItem = (NSMenuItem*)[NSMenuItem separatorItem];
 		// tag this menu item so NSSearchField can use it, by hiding/show it appropriately:
 		[separatorItem setTag:NSSearchFieldRecentsTitleMenuItemTag];
 		[searchMenu insertItem:separatorItem atIndex:5];
 		
-		NSMenuItem* clearItem = [[NSMenuItem alloc] initWithTitle:@"Clear" action:nil keyEquivalent:@""];
+		NSMenuItem *clearItem = [[NSMenuItem alloc] initWithTitle:@"Clear" action:nil keyEquivalent:@""];
 		[clearItem setTag:NSSearchFieldClearRecentsMenuItemTag];	// tag this menu item so NSSearchField can use it
 		[searchMenu insertItem:clearItem atIndex:6];
 		[clearItem release];
@@ -123,37 +121,36 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 	}
 	
 	// build the list of keyword strings for our type completion dropdown list in NSSearchField
-	_builtInKeywords = [[NSMutableArray alloc] initWithObjects:
+	builtInKeywords = [[NSMutableArray alloc] initWithObjects:
 					@"Favorite", @"Favorite1", @"Favorite11", @"Favorite3", @"Vacations1", @"Vacations2",
 					@"Hawaii", @"Family", @"Important", @"Important2",@"Personal", nil];
-	
-	return [super init];
 }
 
 // -------------------------------------------------------------------------------
-//	applicationShouldTerminateAfterLastWindowClosed:sender
+//	applicationShouldTerminateAfterLastWindowClosed:
 //
 //	NSApplication delegate method placed here so the sample conveniently quits
 //	after we close the window.
 // -------------------------------------------------------------------------------
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)sender
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
 {
 	return YES;
 }
 
 
-#pragma mark - custom sheet
+#pragma mark -
+#pragma mark Custom sheet
 
 // -------------------------------------------------------------------------------
-//	sheetDidEnd:sheet:returnCode:contextInfo
+//	sheetDidEnd:sheet:returnCode:returnCode:contextInfo:
 // -------------------------------------------------------------------------------
-- (void)sheetDidEnd:(NSWindow*)sheet returnCode:(int)returnCode contextInfo:(void*)contextInfo;
+- (void)sheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo;
 {
 	[sheet orderOut:self];
 }
 
 // -------------------------------------------------------------------------------
-//	sheetDoneButtonAction:sender
+//	sheetDoneButtonAction:
 // -------------------------------------------------------------------------------
 - (IBAction)sheetDoneButtonAction:(id)sender
 {
@@ -161,15 +158,17 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 }
 
 // -------------------------------------------------------------------------------
-//	actionMenuItem:sender
+//	actionMenuItem:
 // -------------------------------------------------------------------------------
 - (IBAction)actionMenuItem:(id)sender
 {
-	[NSApp beginSheet:simpleSheet modalForWindow:[self window] modalDelegate:self didEndSelector:@selector(sheetDidEnd:returnCode:contextInfo:) contextInfo:nil];
+	[NSApp beginSheet:simpleSheet modalForWindow:[self window] modalDelegate:self
+				didEndSelector:@selector(sheetDidEnd:returnCode:contextInfo:) contextInfo:nil];
 }
 
 
-#pragma mark - keyword search handling
+#pragma mark -
+#pragma mark Keyword search handling
 
 // -------------------------------------------------------------------------------
 //	allKeywords:
@@ -177,37 +176,38 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 //	This method builds our keyword array for use in type completion (dropdown list
 //	in NSSearchField).
 // -------------------------------------------------------------------------------
-- (NSArray*)allKeywords
+- (NSArray *)allKeywords
 {
     NSArray *array = [[[NSArray alloc] init] autorelease];
     unsigned int i,count;
 
-    if (_allKeywords == nil)
+    if (allKeywords == nil)
 	{
-        _allKeywords = [_builtInKeywords mutableCopy];
+        allKeywords = [builtInKeywords mutableCopy];
 
         if (array != nil)
 		{
             count = [array count];
             for (i=0; i<count; i++)
 			{
-                if ([_allKeywords indexOfObject:[array objectAtIndex:i]] == NSNotFound)
-                    [_allKeywords addObject:[array objectAtIndex:i]];
+                if ([allKeywords indexOfObject:[array objectAtIndex:i]] == NSNotFound)
+                    [allKeywords addObject:[array objectAtIndex:i]];
             }
         }
-        [_allKeywords sortUsingSelector:@selector(compare:)];
+        [allKeywords sortUsingSelector:@selector(compare:)];
     }
-    return _allKeywords;
+    return allKeywords;
 }
 
 // -------------------------------------------------------------------------------
-//	control:textView:completions:forPartialWordRange:indexOfSelectedItem
+//	control:textView:completions:forPartialWordRange:indexOfSelectedItem:
 //
 //	Use this method to override NSFieldEditor's default matches (which is a much bigger
 //	list of keywords).  By not implementing this method, you will then get back
 //	NSSearchField's default feature.
 // -------------------------------------------------------------------------------
-- (NSArray*)control:(NSControl*)control textView:(NSTextView *)textView completions:(NSArray*)words forPartialWordRange:(NSRange)charRange indexOfSelectedItem:(int*)index
+- (NSArray *)control:(NSControl *)control textView:(NSTextView *)textView completions:(NSArray *)words
+							forPartialWordRange:(NSRange)charRange indexOfSelectedItem:(int*)index
 {
     NSMutableArray*	matches = NULL;
     NSString*		partialString;
@@ -224,7 +224,9 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 	for (i=0; i< count; i++)
 	{
         string = [keywords objectAtIndex:i];
-        if ([string rangeOfString:partialString options:NSAnchoredSearch | NSCaseInsensitiveSearch range:NSMakeRange(0, [string length])].location != NSNotFound)
+        if ([string rangeOfString:partialString
+						  options:NSAnchoredSearch | NSCaseInsensitiveSearch
+							range:NSMakeRange(0, [string length])].location != NSNotFound)
 		{
             [matches addObject:string];
         }
@@ -235,11 +237,11 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 }
 
 // -------------------------------------------------------------------------------
-//	controlTextDidChange:obj
+//	controlTextDidChange:
 //
 //	The text in NSSearchField has changed, try to attempt type completion.
 // -------------------------------------------------------------------------------
-- (void)controlTextDidChange:(NSNotification*)obj
+- (void)controlTextDidChange:(NSNotification *)obj
 {
 	NSTextView* textView = [[obj userInfo] objectForKey:@"NSFieldEditor"];
 
@@ -256,15 +258,20 @@ Copyright © 2006-2007 Apple Inc. All Rights Reserved
 //
 //	Handle all commend selectors that we can handle here
 // -------------------------------------------------------------------------------
-- (BOOL)control:(NSControl*)control textView:(NSTextView*)textView doCommandBySelector:(SEL)commandSelector
+- (BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector
 {
-    if ([textView respondsToSelector:commandSelector])
+    BOOL result = NO;
+	
+	if ([textView respondsToSelector:commandSelector])
 	{
         commandHandling = YES;
         [textView performSelector:commandSelector withObject:nil];
         commandHandling = NO;
+		
+		result = YES;
     }
-    return YES;
+	
+    return result;
 }
 
 @end
